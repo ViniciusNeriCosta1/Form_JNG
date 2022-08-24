91a7f25bd2151687b281b3afca618d24e79e8501
Vinicius Neri <vinicius.neriii@gmail.com.br>
2022-08-24 10:19:54 -0300

    ajustando estrutura do site

--- retira.php
+++ retira.php
@@ -58,90 +58,94 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Retira | JNG</title>
 </head>
-<body>
-    <div class="fundo_dados">
-        <form action="retira.php" method="POST">
+<header></header>
+<main>
+    <body>
+        <div class="fundo_dados">
+            <form action="retira.php" method="POST">
+                <fieldset>
+                    <legend><b>Formulário de Retira</b></legend>
+                    <br>
+                    <div class="inputBox">
+                        <label for="data_retira" class="labelTime">Data</label>
+                        <input type="date" name="data_retira" id="data_retira" class="inputUser" required>
+                    </div>
+                    <br>
+                    <div class="inputBox">
+                        <label for="time_ent" class="labelTime">Horario Entrada</label>
+                        <input type="time" name="time_ent" id="time_ent" class="inputUser" required>
+                    </div>
+                    <br>
+                    <div class="inputBox">
+                        <input type="text" name="nome" id="nome" class="inputUser" required maxlength="20">
+                        <label for="nome" class="labelInput">Nome</label>
+                    </div>
+                    <br>
+                    <div class="inputBox">
+                        <input type="text" name="empresa" id="empresa" class="inputUser" required maxlength="20">
+                        <label for="empresa" class="labelInput">Empresa</label>
+                    </div>
+                    <br>
+                    <div class="inputBox">
+                        <input type="number" name="doc" id="doc" class="inputUser" required maxlength="11">
+                        <label for="doc" class="labelInput">Documento</label>
+                    </div>
+                    <br>
+                    <div class="inputBox">
+                        <input type="number" name="pedido" id="pedido" class="inputUser" required maxlength="6">
+                        <label for="pedido" class="labelInput">Pedido</label>
+                    </div>
+                    <br></br>
+                    <div class="inputBox">
+                        <input type="text" name="obs" id="obs" class="inputUser" maxlength="20">
+                        <label for="obs" class="labelInput">OBS</label>
+                    </div>
+                    <br>
+                    <input type="submit" name="submit" id="submit">
+                    <br></br>
+                    <button type="submit" name="submit" id="submit">Pesquisar</button>
+                </fieldset>
+            </form>
+        </div>
+        <div class="fundo_table">
             <fieldset>
-                <legend><b>Formulário de Retira</b></legend>
-                <br>
-                <div class="inputBox">
-                    <label for="data_retira" class="labelTime">Data</label>
-                    <input type="date" name="data_retira" id="data_retira" class="inputUser" required>
-                </div>
-                <br>
-                <div class="inputBox">
-                    <label for="time_ent" class="labelTime">Horario Entrada</label>
-                    <input type="time" name="time_ent" id="time_ent" class="inputUser" required>
-                </div>
-                <br>
-                <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" required maxlength="20">
-                    <label for="nome" class="labelInput">Nome</label>
-                </div>
-                <br>
-                <div class="inputBox">
-                    <input type="text" name="empresa" id="empresa" class="inputUser" required maxlength="20">
-                    <label for="empresa" class="labelInput">Empresa</label>
-                </div>
-                <br>
-                <div class="inputBox">
-                    <input type="number" name="doc" id="doc" class="inputUser" required maxlength="11">
-                    <label for="doc" class="labelInput">Documento</label>
-                </div>
-                <br>
-                <div class="inputBox">
-                    <input type="number" name="pedido" id="pedido" class="inputUser" required maxlength="6">
-                    <label for="pedido" class="labelInput">Pedido</label>
-                </div>
-                <br></br>
-                <div class="inputBox">
-                    <input type="text" name="obs" id="obs" class="inputUser" maxlength="20">
-                    <label for="obs" class="labelInput">OBS</label>
-                </div>
-                <br>
-                <input type="submit" name="submit" id="submit">
-                <br></br>
-                <button type="submit" name="submit" id="submit">Pesquisar</button>
+                <legend><b>INFOS</b></legend>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nº Pedido</th>
+                            <th>Nome</th>
+                            <th>Empresa</th>
+                            <th>Documento</th>
+                            <th>Data</th>
+                            <th>Entrada</th>
+                            <th>Saida</th>
+                            <th>OBS</th>
+                            <input type="hidden">
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach ($result as $k => $v) {
+                                echo"<tr>";
+                                echo"<td>".$v['pedido']."</td>";
+                                echo"<td>".$v['nome']."</td>";
+                                echo"<td>".$v['empresa']."</td>";
+                                echo"<td>".$v['doc']."</td>";
+                                echo"<td>".$v['data']."</td>";
+                                echo"<td>".$v['time_ent']."</td>";
+                                echo"<td>
+                                <a href='retira.php?id={$v['id']}' name='informe' id='informe'>Informe</a>
+                                </td>";
+                                echo"<td>".$v['obs']."</td>";
+                                echo"</tr>";
+                            }
+                        ?>    
+                    </tbody>
+                </table>
             </fieldset>
-        </form>
-    </div>
-    <div class="fundo_table">
-        <fieldset>
-            <legend><b>INFOS</b></legend>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nº Pedido</th>
-                        <th>Nome</th>
-                        <th>Empresa</th>
-                        <th>Documento</th>
-                        <th>Data</th>
-                        <th>Entrada</th>
-                        <th>Saida</th>
-                        <th>OBS</th>
-                        <input type="hidden">
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach ($result as $k => $v) {
-                            echo"<tr>";
-                            echo"<td>".$v['pedido']."</td>";
-                            echo"<td>".$v['nome']."</td>";
-                            echo"<td>".$v['empresa']."</td>";
-                            echo"<td>".$v['doc']."</td>";
-                            echo"<td>".$v['data']."</td>";
-                            echo"<td>".$v['time_ent']."</td>";
-                            echo"<td>
-                            <a href='retira.php?id={$v['id']}' name='informe' id='informe'>Informe</a>
-                            </td>";
-                            echo"<td>".$v['obs']."</td>";
-                            echo"</tr>";
-                        }
-                    ?>    
-                </tbody>
-            </table>
-        </fieldset>
-    </div>
-</body>
+        </div>
+    </body>
+</main>
+<footer></footer>
 </html>

b87003a1a22773e6364b0e8f0d9fa15b123b6b09
Vinicius Neri <vinicius.neriii@gmail.com.br>
2022-09-02 17:22:57 -0300

    iniciando tela de pesquisa

---
+++ pesquisar.php
@@ -0,0 +1,89 @@
+<?php
+    date_default_timezone_set('America/Sao_Paulo');
+    include_once('sql/Sql.php');
+
+    $sql = new Sql();
+
+    $result = $sql->query("SELECT * FROM transporte");
+    var_dump($result);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="shortcut icon" type = "imagem/x-icon" href = "./assets/logo_jng.ico"/>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="300">
+    <title>Transporte | JNG</title>
+</head>
+<body>
+    <header>
+        <div class="logo_header">
+            <img src="./assets/logo_JNG_azul.png" alt="Logo JNG" class="img_logo_header">
+        </div>
+        <div class="header-content">
+            <div class="navbar">
+                <a href="./retira.php">Retira</a>
+                <a href="./transporte.php">Transporte</a>
+                <a href="./sedex.php">Sedex</a>
+                <a href="./retira.php">Pesquisa</a>
+            </div>    
+        </div>
+    </header>
+    <main>
+        <div class="pesquisar">
+
+        </div>
+        <div class="fundo_table">
+            <fieldset>
+                <legend><b>INFOS</b></legend>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nº Pedido</th>
+                            <th>NF</th>
+                            <th>Motorista</th>
+                            <th>Data Entrada</th>
+                            <th>Data Saida</th>
+                            <th>OBS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach ($result as $k => $v) {
+                                echo"<tr>";
+                                echo"<td>".$v['pedido']."</td>";
+                                echo"<td>".$v['nf']."</td>";
+                                echo"<td>".$v['motorista']."</td>";
+                                echo"<td>".$v['data_entrada']."</td>";
+                                echo"<td>".$v['data_saida']."</td>";
+                                echo"<td>".$v['obs']."</td>";
+                                echo"</tr>";
+                            }
+                        ?>    
+                    </tbody>
+                </table>
+            </fieldset>
+        </div>
+    </main>
+    <footer>
+        <div class="rodape">
+            <p>Copyright © 2022 Intranet JNG</p>
+        </div>
+    </footer>
+    <script>
+        //funcao para selecionar todos os input tipo number e maxlength funcionar
+        document.querySelectorAll('input[type="number"]').forEach(input =>{
+            input.oninput = () => {
+                if(input.value.length > input.maxLength) input.value = input.value.slice(0, input.maxLength);
+            };
+        });
+    </script>
+</body>
+</html>
+
+
+

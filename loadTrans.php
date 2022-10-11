c5cf20bbc7741e046349ab59716b7a03928ae952
Vinicius Neri <vinicius.neriii@gmail.com.br>
2022-10-11 14:27:01 -0300

    funcao para carregar transportadores do bd

---
+++ loadTrans.php
@@ -0,0 +1,26 @@
+<?php
+    include_once('sql/Sql.php');
+
+    $sql = new Sql();
+
+    $transportadora = filter_input(INPUT_GET, "zb_transportador");
+
+    if(!empty($transportadora)){
+
+        $transp = "%".$transportadora."%";
+        $info = $sql->select('SELECT zb_id, zb_transportador FROM prd_p12.szb WHERE zb_transportador LIKE :zb_transportador LIMIT 2', array(
+            ':zb_transportador' => $transp
+        ));
+        if(empty($info) and count($info) == 0){
+            $retorna = ['erro' => true, 'msg' => "Transportador nao encontrado"];
+        }else{
+            foreach($info as $k => $v){
+                $retorna = ['erro' => false, 'dados' => $info];
+            }
+        }
+    }else{
+        $retorna = ['erro' => true, 'msg' => "Transportador nao encontrado"];
+    }
+
+    echo json_encode($retorna);
+?>

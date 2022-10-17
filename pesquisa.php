a71ac58c3b88e1444c5236b2be7e4231ac1f0742
Vinicius Neri <vinicius.neriii@gmail.com.br>
2022-10-17 15:18:56 -0300

    ajustado escolha de pequisa

--- pesquisa.php
+++ pesquisa.php
@@ -31,7 +31,7 @@
                 <legend><b>Pesquisa</b></legend>
                 <div class="inputPesq">
                     <select name="opcao" id="opcao" onchange="window.location.href=this.value;">
-                        <option value=""></option>
+                        <option value="" disabled></option>
                         <option value="pesquisaRetira.php">Retira</option>
                         <option value="pesquisaSedex.php">Sedex</option>
                         <option value="pesquisaTransporte.php">Transporte</option>

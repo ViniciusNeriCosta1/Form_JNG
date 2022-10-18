c5964188e58eb93f55227993c65ae22545b75359
Vinicius Neri <vinicius.neriii@gmail.com.br>
2022-10-18 17:17:28 -0300

    alterado de if para switch direcionamento de pagina

--- atualiza.php
+++ atualiza.php
@@ -8,7 +8,6 @@ use function PHPSTORM_META\map;
     $sql = new Sql();
     
     if (isset($_GET['za_id']) && !empty($_GET['za_id'])) {
-        /*
         $result = $sql->query('UPDATE prd_p12.sza SET 
         za_hr_saida = :za_hr_saida, za_dt_saida = :za_dt_saida, za_ip = :za_ip WHERE za_id = :za_id', 
         array(
@@ -17,24 +16,20 @@ use function PHPSTORM_META\map;
             ':za_ip' => $_SERVER['REMOTE_ADDR'],
             ':za_id' => $_GET['za_id']
         ));
-        */
         $info = $sql->select('SELECT za_tp_saida FROM prd_p12.sza WHERE za_id = :za_id',
         array(
             ':za_id' => $_GET['za_id'],
         ));
         var_dump($info);
-        if($info[0]['za_tp_saida'] === "retira"){
-            echo "retira";
-            #header('Location: retira.php');
-        }elseif($info[0]['za_tp_saida'] == 'sedex'){
-            echo "sedex";
-            #header('Location: sedex.php');
-        }elseif($info[0]['za_tp_saida'] == 'transporte'){
-            echo "transporte";
-            #header('Location: transporte.php');
-        }else{
-            echo "sair";
-            #header('Location: inserir.php');
+        switch($info[0]['za_tp_saida']){
+            case "retira":
+                header('Location: retira.php');
+            case "sedex":
+                header('Location: sedex.php');
+            case "transporte":
+                header('Location: transporte.php');
+            default:
+                header('Location: inserir.php');
         }
     }
 ?>

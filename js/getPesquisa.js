afcc8076313436d3002e26eaad5fe7bcd70e5a3b
Vinicius Neri <vinicius.neriii@gmail.com.br>
2022-12-15 15:49:22 -0300

    unficado funcao para realizar pesquisa

--- js/getPesquisa.js
+++ js/getPesquisa.js
@@ -3,11 +3,21 @@ var search = document.getElementById('pesquisar');
 search.addEventListener("keydown", function(event) {
     if (event.key === "Enter") 
     {
-        searchDataSedex();
+        searchDataRetira();
     }
 });
+
+function searchDataRetira()
+{
+    window.location = 'pesquisaRetira.php?search='+search.value;
+}
 
 function searchDataSedex()
 {
     window.location = 'pesquisaSedex.php?search='+search.value;
+}
+
+function searchDataTransporte()
+{
+    window.location = 'pesquisaTransporte.php?search='+search.value;
 }

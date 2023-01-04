056f66f5224c6da4e7db9402bb7305dc5eab7b23
Vinicius Neri <vinicius.neriii@gmail.com>
2023-01-04 12:22:15 -0300

    adicionado parametros na funcao

--- js/chancePlaceholder.js
+++ js/chancePlaceholder.js
@@ -1,18 +1,19 @@
-$("#filtro").on("change", function() {
-    var vatExpense = $("#filtro option:selected").val();
+$("#select").on("change", function() {
+    var vatExpense = $("#select option:selected").val();
     var vatPlaceholder = "";
     switch(vatExpense){
         case "za_pedido":
             vatPlaceholder = "Nº Pedido"
+            break
+        case "za_dt_saida":
+            vatPlaceholder = "Ano-Mês-Dia"
             break
         case "za_dt_entrada":
             vatPlaceholder = "Ano-Mês-Dia"
             break
         case "za_nf":
             vatPlaceholder = "Nº NF"
-            break
-        default:
-            vatPlaceholder = "teste"
+            break    
     }
     $("#pesquisar").attr("placeholder", vatPlaceholder);
 });

37dce75f412eb86d485f39ad4b1d3d88f6f3dfb2
Vinicius Neri <vinicius.neriii@gmail.com>
2023-03-01 10:29:06 -0300

    add a classe de erro msg na variavel global msg

--- sql/erro.php
+++ sql/erro.php
@@ -4,6 +4,13 @@ class erro{
     public function erroPedido(){
         echo "<script>alert('Pedido não selecionado');</script>";
     }
+
+    public function notLogin(){
+        if((!isset($_SESSION['ZC_id'])) AND (!isset($_SESSION['ZC_depart']))){
+            header('Location: home.php');
+            $_SESSION['msg'] = "<p style='color: red'>Erro: Necessário realizar o login</p>";
+        }
+    }
 }
 
 ?>
